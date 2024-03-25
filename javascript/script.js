3f25fb1216d0e63585732a683418d91f6fc1fc22
Potamos0713 <potamos0713@gmail.com>
2024-03-25 23:24:23 +0900

    first commit

---
+++ javascript/script.js
@@ -0,0 +1,10 @@
+let weight = 68;
+console.log('入力された体重は' + weight + 'kgです。')
+
+let height = 170;
+console.log('入力された身長は' + height + 'cmです。')
+
+let bmi = weight / ((height / 100) * (height / 100));
+console.log('デバッグ：BMI = ' + bmi);
+
+console.log('入力された体重と身長から導き出されたBMIは約' + ((Math.round(bmi * 10)) / 10) + 'です。');
