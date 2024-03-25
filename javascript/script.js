64152bc70ff2b921c1aeb9164d4afabb0835b371
Potamos0713 <potamos0713@gmail.com>
2024-03-25 23:26:37 +0900

    10行目のMath.roundに関しての説明を追加しました。

--- javascript/script.js
+++ javascript/script.js
@@ -8,3 +8,5 @@ let bmi = weight / ((height / 100) * (height / 100));
 console.log('デバッグ：BMI = ' + bmi);
 
 console.log('入力された体重と身長から導き出されたBMIは約' + ((Math.round(bmi * 10)) / 10) + 'です。');
+
+// 10行目ではBMIを小数第一位まで表示するためMath.roundを利用しています。
